1c93fb46180d5dd52c867db2cc8bb32724032443
Başar <basar.tr@hotmail.com>
2022-04-19 00:57:14 +0300

    Profile Picture Future
    Now you can add a profile picture via 'Edit Image' button on Profile Page.
    It's not permanent for now.

--- PROJECT/src/main/java/module-info.java
+++ PROJECT/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.example.teamder {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.desktop;
+    requires javafx.swing;
 
 
     opens scenes to javafx.fxml;
--- PROJECT/src/main/java/scenes/Main.java
+++ PROJECT/src/main/java/scenes/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Welcome_To_Teamder.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 640, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 640, 400);
         stage.setTitle("Welcome Page");
         stage.setScene(scene);
         stage.show();
@@ -20,4 +20,6 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch();
     }
+
+
 }
