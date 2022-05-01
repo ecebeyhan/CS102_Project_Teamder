e27af5d5c418b526396281ee33893cfd7626429b
Doruk Işık <doruk003@gmail.com>
2022-05-01 21:26:54 +0300

    modifications on position selection

--- PROJECT/src/main/java/scenes/matchPageController.java
+++ PROJECT/src/main/java/scenes/matchPageController.java
@@ -4,10 +4,7 @@ import classes.ImageHandler;
 import classes.Match;
 import classes.User;
 import javafx.embed.swing.SwingFXUtils;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
@@ -17,15 +14,20 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.sql.SQLException;
+import java.util.Optional;
 
 public class matchPageController implements MatchController, MainController{
-
+    @FXML
     public ImageView footballField;
     @FXML
     private Text placeText;
@@ -36,10 +38,14 @@ public class matchPageController implements MatchController, MainController{
     @FXML
     private Text matchNameText;
     @FXML
-    private Button selectPosition1,selectPosition2,selectPosition3,selectPosition4,selectPosition5,selectPosition6,selectPosition7,selectPosition8,selectPosition9,selectPosition10,selectPosition11,selectPosition12;
-
+    private Pane myPane;
+    @FXML
+    private Button selectPosition0,selectPosition1,selectPosition2,selectPosition3,selectPosition4,selectPosition5,selectPosition6,selectPosition7,selectPosition8,selectPosition9,selectPosition10,selectPosition11;
+    private User user;
+    private Match match;
     private File fieldFile; // the image file of the field
 
+    @FXML
     public void clickOnCancel(ActionEvent event) throws IOException {
         SceneChanger sc = new SceneChanger();
         MainController controllerClass = new profileController();
@@ -47,28 +53,61 @@ public class matchPageController implements MatchController, MainController{
     }
 
 
-    //When a user clicks on an available position, a window pops up in order for the user to join the game
+    //When a user clicks on an available position, an alert pops up in order for the user to confirm joining the game
     //When the user confirms, it adds the user to the match in the preferred position
-    public void clickOnSelectPosition(ActionEvent event) throws IOException {
-        Stage window = new Stage();
-        window.initModality(Modality.APPLICATION_MODAL);
+    @FXML
+    public void clickOnSelectPosition(ActionEvent event) throws IOException{
+        Stage stage = (Stage) myPane.getScene().getWindow();
+        Alert.AlertType type = Alert.AlertType.NONE;
+        Alert alert = new Alert(type,"");
 
-        Label label = new Label();
-        label.setText("Join the match in this position");
+        alert.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        alert.getDialogPane().getButtonTypes().add(ButtonType.CLOSE);
 
-        Button joinMatch = new Button();
-        joinMatch.setText("JOIN");
+        alert.initModality(Modality.APPLICATION_MODAL);
+        alert.initOwner(stage);
 
-        //joinMatch butonuna tıklanılınca user ı pozisyona göre maça eklemesi gerekiyor
-        //joinMatch.setOnAction(e -> );
+        alert.getDialogPane().setContentText("Are you sure you want to join the match in this position?");
 
-        VBox layout = new VBox(10);
-        layout.getChildren().addAll(joinMatch);
-        layout.setAlignment(Pos.CENTER);
+        Optional<ButtonType> result = alert.showAndWait();
 
-        Scene scene = new Scene(layout);
-        window.setScene(scene);
-        window.showAndWait();
+        int position = getPositionSelection(event);
+
+        if(result.get() == ButtonType.OK)
+        {
+            Alert infoAlert = new Alert(Alert.AlertType.NONE);
+            infoAlert.getDialogPane().getButtonTypes().add(ButtonType.OK);
+            infoAlert.getDialogPane().lookupButton(ButtonType.OK).setVisible(false);
+            infoAlert.initOwner(stage);
+            infoAlert.getDialogPane().setContentText("You successfully joined the match!");
+            infoAlert.show();
+            //Pozisyona göre eklemiyor
+            /*try {
+
+                Database.addMatch(user,match);
+            }
+            catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }*/
+        }
+    }
+
+    private int getPositionSelection(ActionEvent event)
+    {
+        Button clickedButton = (Button) event.getTarget();
+        if(clickedButton.equals(selectPosition0)){return 0;}
+        if(clickedButton.equals(selectPosition1)){return 1;}
+        if(clickedButton.equals(selectPosition2)){return 2;}
+        if(clickedButton.equals(selectPosition3)){return 3;}
+        if(clickedButton.equals(selectPosition4)){return 4;}
+        if(clickedButton.equals(selectPosition5)){return 5;}
+        if(clickedButton.equals(selectPosition6)){return 6;}
+        if(clickedButton.equals(selectPosition7)){return 7;}
+        if(clickedButton.equals(selectPosition8)){return 8;}
+        if(clickedButton.equals(selectPosition9)){return 9;}
+        if(clickedButton.equals(selectPosition10)){return 10;}
+        if(clickedButton.equals(selectPosition11)){return 11;}
+        return -1; //invalid position
     }
 
     @Override
@@ -77,6 +116,7 @@ public class matchPageController implements MatchController, MainController{
         timeText.setText(match.getStartTime().toString());
         dateText.setText(match.getDate().toString());
         matchNameText.setText(match.getName());
+        this.match = match;
 
         try{
             fieldFile = new File(ImageHandler.IMAGE_PATH + "football_field.jpeg");
