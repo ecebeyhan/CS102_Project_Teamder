1479d47cd0b16d1a40ee32edab6af1e1737dce27
Doruk Işık <doruk003@gmail.com>
2022-05-03 17:35:36 +0300

    minor changes

--- PROJECT/src/main/java/scenes/matchPageController.java
+++ PROJECT/src/main/java/scenes/matchPageController.java
@@ -12,6 +12,7 @@ import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
