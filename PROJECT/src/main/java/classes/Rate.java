7cf9c23b7f230cadcb6e75c7ce18f37c8d694d9d
Başar <basar.tr@hotmail.com>
2022-05-09 19:39:02 +0300

    minor changes

--- PROJECT/src/main/java/classes/Rate.java
+++ PROJECT/src/main/java/classes/Rate.java
@@ -2,36 +2,11 @@ package classes;
 
 public class Rate {
 
-    private int count;
-    private double rating;
-
-    //-----------------------------------------------------------------
-    //  Getter methods
-    //-----------------------------------------------------------------
-    public double getRating() {
-        return rating;
-    }
-
-    public int getCount() {
-        return count;
-    }
-    //-----------------------------------------------------------------
-    //  Setter methods
-    //-----------------------------------------------------------------
-    public void setRating(double rating) {
-        this.rating = rating;
-    }
-
-    public void setCount(int count) {
-        this.count = count;
-    }
-
     /**
      * This method is used to calculate the average rating.
-     * @param rate The rating to be added to the current average.
      */
-    public void update(int rate) {
-        this.rating = (rating * count + rate) / (this.count + 1);
-        setCount(count++);
+    public static double update(double rating, int noOfRaters) {
+        double rate = rating / noOfRaters;
+        return Math.round(rate * 10.0) / 10.0;
     }
 }
--- PROJECT/src/main/java/classes/Sport.java
+++ PROJECT/src/main/java/classes/Sport.java
@@ -2,26 +2,5 @@ package classes;
 
 public abstract class Sport {
 
-    final int TEAM_A = 0;
-    final int TEAM_B = 1;
-
-    protected User[] teamA; // User arrays for both teams
-    protected User[] teamB;
-    protected String image; // uygun classla değişilecek
-
-    public void addPlayer(int team, int position) {
-        if (team == TEAM_A) {
-
-        }
-    }
-
-    public void removePlayer(int team, int position) {
-
-    }
-
-    public void setTeams(int playerCount) {
-
-    }
-
     public abstract String getName();
 }
